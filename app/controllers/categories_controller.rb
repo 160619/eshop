ba895a563ae85b0a1dcecb0a353438e77084d8f8
“nvtamnt99” <nvtamnt99@gmail.com>
2020-10-04 09:52:09 +0700

    refactor code 2 controller: category, product

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,4 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @categories = Category.all
     @categories = Category.search(params[:search])
